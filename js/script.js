29972c75e225ea81e4b4612bc2f81df5cc1212a0
Evgeniy Kulikov <evgenii49-9@yndex.ru>
2020-12-01 01:08:45 +0300

    Структура кода

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 "use strict";
 const calcc = () =>{
-
-    
-
 jsl(function(){
 	function loadStatus(){
 		var status=jsl('[name="onoffswitch"]').prop('checked');
@@ -124,17 +121,14 @@ jsl(function(){
 
 
 // модальное окно
-const togglePopUp = () => {
+const call = () => {
     const popupCall = document.querySelector('.popup-call');
       let callBtn = document.querySelectorAll('.call-btn');
-      let discountBtn = document.querySelectorAll('.discount-btn');
-      let  popupDiscount = document.querySelector('.popup-discount');
-   
         // первое модальное окно
       callBtn.forEach((elem) => {
     elem.addEventListener('click', () => {
         if(popupCall.style.opacity = '0'){
-          
+  
             let n = 0;
             const timer = setInterval(() => { 
                 n += 0.1;
@@ -195,138 +189,149 @@ popupCall.addEventListener('click', (event) => {
 
 // обработка кнопки Заказать со скидкой(2- модальное)
 
-discountBtn.forEach((elem) => {
-    elem.addEventListener('click', () => {
-        if(popupDiscount.style.opacity = '0'){
-            
-            let n = 0;
-            const timer = setInterval(() => { 
-                n += 0.1;
-                popupDiscount.style.opacity = `${n}`;
-    //  останавливаем таймер
-                if (popupDiscount.style.opacity === '1') {
-                    clearInterval(timer);
-                }
-
-            }, 40);
-            popupDiscount.style.display = 'block';
-            
-        }else{
-            popupDiscount.style.display = 'block';
-            popupDiscount.style.opacity = '1';
-        }
-    });
-}); 
-const formName1 = document.getElementById('name_1'),
-formPhone1 = document.getElementById('phone_1');
-
-
-// закрытие окна
-popupDiscount.addEventListener('click', (event) => {
-    let target = event.target;
-
-   
-    if(target.classList.contains('popup-close', 'popup-call popup')){
-        if(popupDiscount.style.opacity = '1'){
-            let n = 1;
-            const timer = setInterval(() => {
-                n-= 0.1;
-                popupDiscount.style.opacity = `${n}`;
-
-                if (popupDiscount.style.opacity === '-0.1') {
-                    // очищение полей
-                    formName.value = '';
-                    formPhone.value = '';
-                    popupDiscount.style.display = 'none';
-                    clearInterval(timer);
-                }
-
-            }, 40);
-            
-        }else{
-            popupDiscount.style.display = 'none';
-        }
-    }else {
-        target = target.closest('.popup-content');
-        if(!target){
-            popupDiscount.style.display = 'none';
-            formName1.value = '';
-             formPhone1.value = '';
-        }
-    }
-});
-
  let consultationBtn = document.querySelector('.consultation-btn');
 
 // check Третье модально окно
-let check = document.querySelector('.check-btn'),
-        popupCheck = document.querySelector('.popup-check');
-       
-check.addEventListener('click', () => {
-        
-        if(popupCheck.style.opacity === '0'){
-             
-            let n = 0;
-            const timer = setInterval(() => { 
-                n += 0.1;
-                popupCheck.style.opacity = `${n}`;
-    //  останавливаем таймер
-                if (popupCheck.style.opacity === '1') {
-                    clearInterval(timer);
-                }
-
-            }, 40);
-            popupCheck.style.display = 'block';
-            
-        }else{
-            popupCheck.style.display = 'block';
-            popupCheck.style.opacity = '1';
-        }
-    });
-
-    const formName2 = document.getElementById('name_12'),
-formPhone2 = document.getElementById('phone_12');
-    // закрываем модалку
-    popupCheck.addEventListener('click', (event) => {
-        let target = event.target;
-    
-       
-        if(target.classList.contains('popup-close', 'popup-call popup')){
-            if(popupCheck.style.opacity = '1'){
-                
-                let n = 1;
-                const timer = setInterval(() => {
-                    n-= 0.1;
-                    popupCheck.style.opacity = `${n}`;
-    
-                    if ( popupCheck.style.opacity === '-0.1') {
-                        // очищение полей
-                        formName2.value = '';
-                        formPhone2.value = '';
-                        popupCheck.style.display = 'none';
-                        clearInterval(timer);
-                    }
-    
-                }, 40);
-                
-            }else{
-                popupCheck.style.display = 'none';
-            }
-        }else {
-            target = target.closest('.popup-content');
-            if(!target){
-                popupCheck.style.display = 'none';
-                formName2.value = '';
-                 formPhone2.value = ''
-            }
-        }
-    });
 
     
 
 
 
 }
+
+// дисконт 
+const discontModal = () =>{
+    let discountBtn = document.querySelectorAll('.discount-btn');
+    let  popupDiscount = document.querySelector('.popup-discount');
+    discountBtn.forEach((elem) => {
+        elem.addEventListener('click', () => {
+            if(popupDiscount.style.opacity = '0'){
+                
+                let n = 0;
+                const timer = setInterval(() => { 
+                    n += 0.1;
+                    popupDiscount.style.opacity = `${n}`;
+        //  останавливаем таймер
+                    if (popupDiscount.style.opacity === '1') {
+                        clearInterval(timer);
+                    }
+    
+                }, 40);
+                popupDiscount.style.display = 'block';
+                
+            }else{
+                popupDiscount.style.display = 'block';
+                popupDiscount.style.opacity = '1';
+            }
+        });
+    }); 
+    const formName = document.getElementById('name_1'),
+    formPhone = document.getElementById('phone_1');
+    
+    
+    // закрытие окна
+    popupDiscount.addEventListener('click', (event) => {
+        let target = event.target;
+    
+       
+        if(target.classList.contains('popup-close', 'popup-call popup')){
+            if(popupDiscount.style.opacity = '1'){
+                let n = 1;
+                const timer = setInterval(() => {
+                    n-= 0.1;
+                    popupDiscount.style.opacity = `${n}`;
+    
+                    if (popupDiscount.style.opacity === '-0.1') {
+                        // очищение полей
+                        formName.value = '';
+                        formPhone.value = '';
+                        popupDiscount.style.display = 'none';
+                        clearInterval(timer);
+                    }
+    
+                }, 40);
+                
+            }else{
+                popupDiscount.style.display = 'none';
+            }
+        }else {
+            target = target.closest('.popup-content');
+            if(!target){
+                popupDiscount.style.display = 'none';
+                formName.value = '';
+                 formPhone.value = '';
+            }
+        }
+    });
+    
+    };
+
+    const  sale = () =>{
+        let check = document.querySelector('.check-btn'),
+                popupCheck = document.querySelector('.popup-check');
+               
+        check.addEventListener('click', () => {
+                
+                if(popupCheck.style.opacity === '0'){
+                     
+                    let n = 0;
+                    const timer = setInterval(() => { 
+                        n += 0.1;
+                        popupCheck.style.opacity = `${n}`;
+            //  останавливаем таймер
+                        if (popupCheck.style.opacity === '1') {
+                            clearInterval(timer);
+                        }
+        
+                    }, 40);
+                    popupCheck.style.display = 'block';
+                    
+                }else{
+                    popupCheck.style.display = 'block';
+                    popupCheck.style.opacity = '1';
+                }
+            });
+        
+            const formName2 = document.getElementById('name_12'),
+        formPhone2 = document.getElementById('phone_12');
+            // закрываем модалку
+            popupCheck.addEventListener('click', (event) => {
+                let target = event.target;
+            
+               
+                if(target.classList.contains('popup-close', 'popup-call popup')){
+                    if(popupCheck.style.opacity = '1'){
+                        
+                        let n = 1;
+                        const timer = setInterval(() => {
+                            n-= 0.1;
+                            popupCheck.style.opacity = `${n}`;
+            
+                            if ( popupCheck.style.opacity === '-0.1') {
+                                // очищение полей
+                                formName.value = '';
+                                formPhone.value = '';
+                                popupCheck.style.display = 'none';
+                                clearInterval(timer);
+                            }
+            
+                        }, 40);
+                        
+                    }else{
+                        popupCheck.style.display = 'none';
+                    }
+                }else {
+                    target = target.closest('.popup-content');
+                    if(!target){
+                        popupCheck.style.display = 'none';
+                        formName.value = '';
+                         formPhone.value = ''
+                    }
+                }
+            });
+        
+        };
+
 // кнопка больше
 const newClass = () => {
     const buttonMore =  document.querySelector('.add-sentence-btn')
@@ -465,14 +470,18 @@ for(let i = 0; i < form.length; i++) {
 		let target = event.target; 
 		
 		if(target.matches('.phone-user')){
-			if (target.value.length > 5 && target.value.length != '' ){
-				if (target.value.length > 15 && target.value.length != '' ){
+			if (target.value.length > 15 && target.value.length != '' ){
+				if (target.value.length > 15 && target.value.length != ''  ){
                     alert('введите правильный номер') ;
                     target.value = '';
                   }   
             }   
             target.value = target.value.replace(/[^+0-9]/gi, '');
-		}  
+        }  
+        // if  (target.value.length < 7){
+        //     alert('введите правильный номер') ;
+        //     target.value = '';
+        // }
 		 
 		 });
 
@@ -527,8 +536,10 @@ for(let i = 0; i < form.length; i++) {
 
 
 // запуск функций
-	togglePopUp();
+	call();
 	newClass();
 	sendForm();
     calcc();
     helpConsul();
+    discontModal();
+    sale();
